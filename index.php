028a2f2b0f74db42481807d687126228b812a03a
Spencer Bravo <spencer@techandcode.com>
2014-10-20 23:05:05 -0700

    added very basic index(just includes /inc files

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT']."/inc/config.inc.php");
+include_once($_SERVER['DOCUMENT_ROOT']."/inc/header.inc.php");
+
+
+include_once($_SERVER['DOCUMENT_ROOT']."/inc/footer.inc.php");
+?>
+
